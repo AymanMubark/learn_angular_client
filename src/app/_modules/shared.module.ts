39e7beb2e958f1dcc9930a7a290cb5017adca758
Ayman Mubarak <cs.aymanmubark@gmail.com>
2022-07-23 22:45:09 +0300

    add update profile

--- src/app/_modules/shared.module.ts
+++ src/app/_modules/shared.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
-
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    TabsModule.forRoot(),
     BsDropdownModule.forRoot(),
+    NgxGalleryModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     })
   ],
   exports:[
     BsDropdownModule,
-    ToastrModule
+    TabsModule,
+    ToastrModule,
+    NgxGalleryModule
   ]
 })
 export class SharedModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,24 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  users:any;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToggle(){
     this.registerMode = !this.registerMode;
   }
 
-  getUsers() {
-    this.http.get("https://localhost:7222/api/users").subscribe((response?) => {
-      this.users = response;
-    }, (error) => {
-      console.log(error);
-    });
-  }
 
   cancelRegisterMode(event: boolean){
     this.registerMode =event;
